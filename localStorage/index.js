45b063749101ebc6dce07faf442830e515ff21c8
Tom Maeckelberghe <tom.maeckelberghe@persgroep.be>
2020-01-02 17:22:51 +0100

    Create keepalive to check other pages in ios safari

--- localStorage/index.js
+++ localStorage/index.js
@@ -1,37 +1,60 @@
 // localStorage inside RadioPlayer webview on android is null and does not allow polyfill.
 const localStorage = {
-  setItem: function(id, val) {
+  setItem (id, val) {
     try {
-      return window.localStorage.setItem(id, val);
+      return window.localStorage.setItem(id, val)
     } catch (err) {
-      console.error(err);
-      return;
+      console.error(err)
+      return
     }
   },
-  getItem: function(id) {
+  getItem (id) {
     try {
-      return window.localStorage.getItem(id);
+      return window.localStorage.getItem(id)
     } catch (err) {
-      console.error(err);
-      return;
+      console.error(err)
+      return
     }
   },
-  removeItem: function(id) {
+  removeItem (id) {
     try {
-      return window.localStorage.removeItem(id);
+      return window.localStorage.removeItem(id)
     } catch (err) {
-      console.error(err);
-      return;
+      console.error(err)
+      return
     }
   },
-  clear: function() {
+  clear () {
     try {
-      return window.localStorage.clear();
+      return window.localStorage.clear()
     } catch (err) {
-      console.error(err);
-      return;
+      console.error(err)
+      return
     }
   }
-};
+}
 
-export { localStorage as default };
+let lastPing
+
+localStorage.keepalive = {
+  check (id, { onAlive, onDead }) {
+    window.addEventListener('storage', () => {
+      if( localStorage.getItem(id) ){
+        lastPing = (new Date()).getTime()
+      }
+    })
+    setInterval(() => {
+      const two_seconds_ago = (new Date() - 2000)
+      if(!lastPing || (lastPing < two_seconds_ago)){
+        onDead && onDead()
+      } else {
+        onAlive && onAlive()
+      }
+      localStorage.removeItem(id)
+    }, 1000)
+  },
+  startPinging (id) {
+    setInterval(() => localStorage.setItem(id, true), 300)
+  }
+}
+export { localStorage as default }
